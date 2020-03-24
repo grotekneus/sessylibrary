d93467aeaf3aaf611dbde6c7955c3327f028cf83
wgroeneveld <wouter.groeneveld@gmail.com>
2020-03-24 10:27:49 +0100

    iets kapot gemaakt ter demonstratie

--- src/main/java/be/kuleuven/sessylibrary/api/BorrowBooksResource.java
+++ src/main/java/be/kuleuven/sessylibrary/api/BorrowBooksResource.java
@@ -34,15 +34,7 @@ public class BorrowBooksResource extends BooksResource {
     @GET
     public Response borrow(@QueryParam("isbn") Optional<Integer> isbn, @Context HttpServletRequest request) {
         var book = booksRepository.findBookByIsbn(isbn.orElse(6666));
-        if(book == null) {
-            throw new UnsupportedOperationException("Book with isbn " + isbn + " not found!");
-        }
-
-        try {
-            bookLendService.lend(book, requireUserFrom(request));
-        } catch(UnsupportedOperationException oe) {
-            return Response.status(Response.Status.FORBIDDEN).build();
-        }
+        // extra code balblabla
         return Response.ok().build();
     }
 
